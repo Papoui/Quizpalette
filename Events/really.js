28b5a1ac4b939117c39c6ba8857c5496276d5af2
Pap <darckrim64@gmail.com>
2026-08-10 23:03:24 +0200

    Update de really pour prendre en compte plusieurs mots d'une phrase

--- Events/really.js
+++ Events/really.js
@@ -4,7 +4,7 @@ Le bot reprends le mot trouvé entre guillemets et affiche un gif représentant 
 WIP: pour chaque correspondance, 
 */
 const { Events, EmbedBuilder } = require('discord.js');
-const { words } = require('./reallyList.json')
+const { words: wordList } = require('./reallyList.json')
 const { klipyApiKey } = require('../config.json');
 
 module.exports = {
@@ -12,22 +12,24 @@ module.exports = {
     async execute(message) {
         if (message.author.bot) return;
         const sentence = message.content.toLowerCase().split(/\s+/);
-        for(const word of words){
-            const found = sentence.find(f => f.includes(word));
-            if(found){
-                gif = await fetch("https://api.klipy.com/api/v1/TVwY0L8Miwootj7MARl0IdC4R19imf7fy7yfyv7U7hMpkkQigTkHSFJ3Ju4HpfgZ/gifs/search?page=1&per_page=50&q=unimpressed&customer_id={customer_id}&locale={country_code}&content_filter={content_filter}");
-                gif = await gif.json();
-                gif = gif.data.data[Math.floor(Math.random() * gif.data.data.length)].file.hd.gif.url;
-                message.channel.reply({ 
-                    embeds: [
-                        new EmbedBuilder()
-                        .setColor(0xFF0090)
-                        .setTitle("\""+found+"\"")
-                        .setImage(gif)
-                    ]
-                })
-                .catch(console.log("Erreur lors de la réponse."));
-            }
+        const foundWordsList = [];
+        for (const word of wordList) {
+            foundWordsList.push(...sentence.filter(f => f.includes(word)).map(foundWord => ({ word, foundWord })));
+        }
+            
+        if(foundWordsList.length>0){
+            const list = [...new Set(foundWordsList.map(item => item.foundWord))].join(", ");
+            gif = await fetch(`https://api.klipy.com/api/v1/${klipyApiKey}/gifs/search?page=1&per_page=50&q=unimpressed&customer_id={customer_id}&locale={country_code}&content_filter={content_filter}`);
+            gif = await gif.json();
+            gif = gif.data.data[Math.floor(Math.random() * gif.data.data.length)].file.hd.gif.url;
+            message.reply({ 
+                embeds: [
+                    new EmbedBuilder()
+                    .setColor(0xFF0090)
+                    .setTitle("\""+list+"\"")
+                    .setImage(gif)
+                ]
+            })
         }
     }
 }
